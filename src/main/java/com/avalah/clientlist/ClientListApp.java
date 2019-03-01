73c1da0fc2094bbe0222e89c2bfd50446fd57ca3
Mustafa  Tufan <mail@mustafatufan.com.tr>
2019-03-02 02:08:25 +0300

    initial

---
+++ src/main/java/com/avalah/clientlist/ClientListApp.java
@@ -0,0 +1,13 @@
+package com.avalah.clientlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientListApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientListApp.class, args);
+	}
+
+}
