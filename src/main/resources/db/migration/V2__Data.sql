3a0352616c499dfd28fec6955f83509f28048829
Mustafa  Tufan <mail@mustafatufan.com.tr>
2019-03-03 21:12:45 +0300

    flyway

--- src/main/resources/db/migration/V2__Data.sql
+++ src/main/resources/db/migration/V2__Data.sql
@@ -1,4 +1,3 @@
--- password: avalah
 insert into user (username, password)
 values ('egonraamat', '$2a$10$.OLAWuFla7VgmDLkNkZeaOdFZrEKGxMwHlzCpa0wjq.90Lp84XJvC');
 insert into user (username, password)
